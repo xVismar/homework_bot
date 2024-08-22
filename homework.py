bc76efb70a0474d2103180ae395c62d29e781e4d
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-08-23 01:25:55 +0300

    Исправления после Ревью 1
    В некоторых местах (помечены # комментарием) не возможно было заменить переменную или вызов - Pytest не пропускал.
    Обновления переменных.
    Был добавлен кастомных Handler и фильтр для logging.
    Иные изменения, согласно ревью.

--- homework.py
+++ homework.py
@@ -1,45 +1,67 @@
 import json
 import logging
 import os
+import sys
 import time
+from datetime import datetime
 from http import HTTPStatus
-from logging.handlers import RotatingFileHandler
 
+import requests
 from dotenv import load_dotenv
 from telebot import TeleBot
 from telebot.apihelper import ApiException
-import requests
+from telebot.handler_backends import ContinueHandling
+from xmlrpc.client import ResponseError
+
 
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('YA_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('MASTER_ID')
-
-RETRY_PERIOD = 600
+POLLING_REFRESH_PERIOD_SECONDS = 600  # 600 Секунд - 10 минут
+RETRY_PERIOD = POLLING_REFRESH_PERIOD_SECONDS  # @Pytest
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+
+# Фильтр для сообщений\уровней информации и разделение метода вывода информации
+# Оригинал кода - https://stackoverflow.com/a/28743317
+class LogFilter(logging.Filter):
+    """Фильтр (пропускающий) сообщений уровня ниже LEVEL."""
+
+    def __init__(self, level):
+        self.level = level
+
+    def filter(self, record):
+        return record.levelno < self.level
+
+
+MIN_LEVEL = logging.INFO
+stdout_handler = logging.StreamHandler(sys.stdout)
+stderr_handler = logging.StreamHandler(sys.stderr)
+log_filter = LogFilter(logging.WARNING)
+stdout_handler.addFilter(log_filter)
+stdout_handler.setLevel(MIN_LEVEL)
+stderr_handler.setLevel(max(MIN_LEVEL, logging.WARNING))
+
 if __name__ == '__main__':
     logging.basicConfig(
-        level=logging.DEBUG,
         filename=os.path.expanduser('~/program.log'),
         format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
         filemode='w',
-        encoding='utf-8'
+        encoding='utf-8',
     )
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = RotatingFileHandler(
-    'program.log', maxBytes=50000000, backupCount=5)
-logger.addHandler(handler)
+    rootLogger = logging.getLogger()
+    rootLogger.addHandler(stdout_handler)
+    rootLogger.addHandler(stderr_handler)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
 
 def check_tokens():
@@ -48,39 +70,39 @@ def check_tokens():
     for token in tokens:
         if not token:
             logging.critical(f'Отсутствует {token}.')
-            return False
-    logging.debug('Все токены получены успешно.')
-    return all(tokens)
+            raise
+    logging.info('Все токены получены успешно.')
+    return tokens
 
 
 def send_message(bot, message):
     """Отправляет сообщение в Telegram чат."""
+    logging.info('Сообщение отправляется.')
     try:
-        logging.info('Сообщение отправляется.')
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        # сообщение с уровнем debug = @Pytest
         logging.debug('Сообщение успешно отправлено.')
     except ApiException as error:
+        # Можно поменять общий APIException на APITelegramException, но @Pytest
         logging.error(error, exc_info=True)
-        logging.debug(f'Сообщение не отправлено - {error}.')
+        logging.error(f'Сообщение не отправлено - {error}.')
+        return ContinueHandling()
 
 
 def get_api_answer(timestamp):
     """Делает запрос к API."""
+    logging.info('Отправляю запрос к API.')
+    assert isinstance(timestamp, int), (
+        'Дата передана в запрос в неверном формате.'
+    )
+    payload = {'from_date': timestamp}
+
     try:
-        if not isinstance(timestamp, int):
-            raise TypeError
-        payload = {'from_date': timestamp}
-        logging.info('Отправляю запрос к API.')
-        response = requests.get(
-            ENDPOINT, headers=HEADERS, params=payload)
+        response = requests.get(ENDPOINT, headers=HEADERS, params=payload)
         if response.status_code != HTTPStatus.OK:
-            logging.error('API вернул код, отличный от 200.')
-            raise requests.exceptions.HTTPError
-        return response.json()
-    except json.JSONDecodeError as error:
-        logging.error(f'Не удалось обработать JSON {error}.')
-        return None
+            raise requests.exceptions.HTTPError(response)
     except requests.exceptions.RequestException as error:
+        logging.error('API вернул код, отличный от 200.')
         errors = (
             (400, 'некорректный запрос к API'),
             (420, 'слишком много запросов'),
@@ -89,42 +111,47 @@ def get_api_answer(timestamp):
         )
         for status, message in errors:
             if error.status_code == status:
-                logging.error(f'Ошибка отправки запроса - {message} {status}.')
-                raise KeyError
-    except Exception:
-        raise TypeError
+                logging.debug(
+                    f'API вернул статус {error.status_code}'
+                    f' - {message}'
+                )
+    try:
+        response.json()
+    except json.JSONDecodeError as error:
+        logging.error(f'Не удалось обработать JSON {error}.')
+    return response.json()
 
 
 def check_response(response):
     """Проверяет ответ API."""
     if not response:
         logging.error('Нет ответа от сервера.')
-        raise Exception
+        raise ResponseError
     if not isinstance(response, dict):
         logging.error('Ответ от сервера - не словарь.')
         raise TypeError
     if 'homeworks' not in response:
         logging.error('В ответе API отсутствует ключ - домашние работы.')
         raise KeyError
-    if not isinstance(response['homeworks'], list):
+    homeworks = response['homeworks']
+    if not isinstance(homeworks, list):
         logging.error('В ответе API домашние работы - не список.')
         raise TypeError
-    return response['homeworks']
+    return homeworks
 
 
 def parse_status(homework):
     """Получения статуса домашней работы."""
-    logging.debug('Начинаю парсинг.')
+    logging.info('Начинаю парсинг.')
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    if all([homework_name, homework_status]):
-        verdict = HOMEWORK_VERDICTS.get(homework_status)
-        if not verdict:
-            raise ValueError(
-                f'Недокументированный '
-                f'статус домашней работы - {homework_status}.')
-    else:
+    if not all((homework_name, homework_status,)):
         raise KeyError('В ответе API отсутствует имя работы или статус.')
+    verdict = HOMEWORK_VERDICTS.get(homework_status)
+    if not verdict:
+        raise ValueError(
+            f'Недокументированный '
+            f'статус домашней работы - {homework_status}.')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -135,20 +162,25 @@ def main():
         raise SystemExit(-1)
     bot = TeleBot(token=TELEGRAM_TOKEN)
     send_message(bot, 'Старт бота')
-    timestamp1 = 1
-    timestamp2 = 0
+    timestamp_1 = 1
+    timestamp_2 = 0
     while True:
         try:
-            response = get_api_answer(timestamp1)
-            homework = check_response(response)
-            timestamp2 = response.get('current_date')
-            if homework and timestamp1 != timestamp2:
-                timestamp1 = timestamp2
-                message = parse_status(homework[0])
-                send_message(bot, message)
+            parsed_response_content = get_api_answer(timestamp_1)
+            homeworks = check_response(parsed_response_content)
+            timestamp_2 = (
+                parsed_response_content.get('current_date') if True
+                else datetime.today()
+            )
+            if homeworks and timestamp_1 != timestamp_2:
+                for homework in homeworks:
+                    timestamp_1 = timestamp_2
+                    message = parse_status(homework)
+                    send_message(bot, message)
         except Exception as error:
             message = f'Сбой в работе программы: {error}.'
-            send_message(bot, message)
+            if error != ApiException:
+                send_message(bot, message)
             logging.error(message)
         finally:
             time.sleep(RETRY_PERIOD)
